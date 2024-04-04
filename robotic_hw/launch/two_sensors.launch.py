9c442d9fcd6e9d7cd3c8f8c78c1509f10b9740b6
Priyanshu Agrawal <priyanshu.agrawal@uconn.edu>
2024-04-03 21:19:56 -0400

    Created launch file and uncommented second sensor

---
+++ robotic_hw/launch/two_sensors.launch.py
@@ -0,0 +1,50 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    return LaunchDescription([
+        
+        # Launch the first server node for the first sensor
+        # The number of samples and request_frequency parametes are chosen based on sensor sample rate and delay
+        # I choose them such that more time is spent on sampling than the delay
+        # The first sensor has a sample rate of 2000 Hz and a delay of 1 ms
+        Node(
+            package='robotic_hw',
+            executable='load_cell_data_server',
+            name='load_cell_data_server_1',
+            output='screen',
+            namespace='sensor_1',
+            parameters=[{'server_address': '127.0.0.3'},
+                        {'server_port': 10000},
+                        {'number_of_samples': 5},
+                        {'request_frequency': 200.0}]),
+        
+        # Launch the second  server node for the second sensor
+        # The second sensor has a sample rate of 4000 Hz and a delay of 3 ms
+        # Because the delay is longer, the number of samples is increased and sampling frequency is decreased
+        Node(
+            package='robotic_hw',
+            executable='load_cell_data_server',
+            name='load_cell_data_server_2',
+            output='screen',
+            namespace='sensor_2',
+            parameters=[{'server_address': '127.0.0.1'},
+                        {'server_port': 10000},
+                        {'number_of_samples': 16},
+                        {'request_frequency': 125.0}]),
+
+        # Launch the load cell data publisher node
+        Node(
+            package='robotic_hw',
+            executable='load_cell_data_publisher',
+            name='load_cell_data_publisher',
+            output='screen',
+            parameters=[{'service_names': ['sensor_1/get_load_cell_data', 'sensor_2/get_load_cell_data']},
+                        {'publish_frequency': 500.0},
+                        {'service_call_frequency': 200.0}])
+
+    ])
--- robotic_hw/setup.py
+++ robotic_hw/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'robotic_hw'
 
@@ -7,9 +9,11 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        ('share/' + package_name, ['package.xml']),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +24,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'load_cell_data_server = robotic_hw.load_cell_data_server:main',
+            'load_cell_data_publisher = robotic_hw.load_cell_data_publisher:main',
         ],
     },
 )
